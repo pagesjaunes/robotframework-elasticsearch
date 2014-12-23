601a2269a74398c93bf7805cb472380f6bbce949
fdepaulis <fdepaulis.externe@pagesjaunes.fr>
2014-12-23 09:33:05 +0100

    Setup.py that rocks

--- setup.py
+++ setup.py
@@ -2,21 +2,23 @@
 
 """Setup script for Robot's ElasticSearch distributions"""
 
-from distutils.core import setup
-import sys, os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
-from ElasticSearchLib import __version__
+# from distutils.core import setup
+# import sys, os
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+from setuptools import setup, find_packages
+import ElasticSearchLib
 
 def main():
 	setup(name = 'robotframework-elasticsearch',
-		version = __version__,
+		version = ElasticSearchLib.__version__,
 		description = 'ElasticSearch library for Robot Framework',
 		author = 'PagesJaunes',
 		author_email = 'fdepaulis@pagesjaunes.fr',
 		url = 'https://github.com/pagesjaunes/robotframework-elasticsearch',
-		package_dir = { '' : 'src'},
-		packages = ['ElasticSearchLib'],
-		requires = ['robotframework','elasticsearch']
+		# package_dir = { '' : 'src'},
+		packages=find_packages(),
+		# packages = ['ElasticSearchLib'],
+		install_requires = ['robotframework','elasticsearch']
 	)
 
 if __name__ == "__main__":
--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from ElasticSearchLib import ElasticSearchLib
-
-class ElasticSearchLibrary(ElasticSearchLib):
-       
-    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
-    __version__ = '1.0'
-
